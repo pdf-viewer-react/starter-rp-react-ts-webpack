e1f1a875ca2b7e90a641aedafa5406a9e8579501
terawuth.th <terawuth.th@logicspark.com>
2025-02-28 16:50:29 +0700

    update webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,17 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.pdf$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }
+        ]
       }
     ],
   },
